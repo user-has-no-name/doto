5f776a1f16808766f976648fa8dadfa902ef062b
Oleksandr Zavazhenko <zavazhenski@gmail.com>
2021-11-30 17:58:56 +0100

    Initial Commit

---
+++ doto/dotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  dotoApp.swift
+//  doto
+//
+//  Created by Oleksandr Zavazhenko on 30/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct dotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
